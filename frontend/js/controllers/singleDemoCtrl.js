954f9053279162d73ca2e44d20909239b1e12be0
Zac McClung <zac.mcclung370@gmail.com>
2018-02-03 21:52:27 -0700

    editing singleDemo view

--- frontend/js/controllers/singleDemoCtrl.js
+++ frontend/js/controllers/singleDemoCtrl.js
@@ -1,22 +1,21 @@
-angular.module("myApp").controller('singleDemoCtrl', function($scope, mainSrvc, $stateParams, $rootScope) {
-
+angular.module('myApp').controller('singleDemoCtrl', function($scope, mainSrvc, $stateParams, $rootScope) {
   $scope.user = $rootScope.loggedUser;
 
   $scope.getSingleDemo = () => {
     mainSrvc.getSingleDemo($stateParams.id).then(function(response) {
-      $scope.singleDemo = response;
+      $scope.singleDemo = response.data[0];
       console.log($scope.singleDemo);
     });
-  }
+  };
   $scope.getSingleDemo();
 
   if ($rootScope.loggedUser) {
-    console.log("!!!im working!!!")
-    $("#login").hide();
-    $("#register").hide()
+    console.log('!!!im working!!!');
+    $('#login').hide();
+    $('#register').hide();
   } else {
-    console.log("!!!im not working!!!")
-    $("#home_greeting").hide();
-    $("#account").hide();
+    console.log('!!!im not working!!!');
+    $('#home_greeting').hide();
+    $('#account').hide();
   }
-})
+});
--- frontend/js/service/mainSrvc.js
+++ frontend/js/service/mainSrvc.js
@@ -1,20 +1,19 @@
 angular.module('myApp').service('mainSrvc', function($http, $rootScope) {
-
   this.getAllEntries = () => {
     return $http({
       method: 'GET',
       url: '/api/entries'
-    }).then(response => response.data)
+    }).then(response => response.data);
   };
 
-  this.addEntry = (entry) => {
+  this.addEntry = entry => {
     return $http({
       method: 'POST',
       url: '/api/demo',
-      data: {entry}
+      data: { entry }
     }).then(response => {
       return response.data.entry;
-    })
+    });
   };
 
   // this.getSingleProduct = (param) => {
@@ -26,16 +25,16 @@ angular.module('myApp').service('mainSrvc', function($http, $rootScope) {
     return $http({
       method: 'GET',
       url: '/api/singleDemo/' + param + '/'
-    }).then(response => response.data[0])
-  }
+    }).then(response => response);
+  };
 
-  this.register = (user) => {
-    console.log(user)
+  this.register = user => {
+    console.log(user);
     return $http({
       method: 'POST',
       url: '/api/register',
       data: user
-    }).then(response => response)
+    }).then(response => response);
   };
 
   this.login = (email, password) => {
@@ -52,10 +51,8 @@ angular.module('myApp').service('mainSrvc', function($http, $rootScope) {
   this.logOut = () => {
     return $http({
       method: 'GET',
-      url: "/logout"
-    }).then(response => {
-
-    });
+      url: '/logout'
+    }).then(response => {});
   };
 
   this.checkLoginStatus = () => {
@@ -63,29 +60,29 @@ angular.module('myApp').service('mainSrvc', function($http, $rootScope) {
       method: 'GET',
       url: '/loggedUser'
     }).then(response => {
-      if(response.status === 200){
+      if (response.status === 200) {
         return response.data;
       } else {
-        return
+        return;
       }
-    })
+    });
   };
 
-  this.getUserEntries = (user) => {
+  this.getUserEntries = user => {
     // console.log("user", user);
     return $http({
-      method: "POST",
+      method: 'POST',
       url: '/api/account',
       data: user
-    }).then(response => response.data)
+    }).then(response => response.data);
   };
 
   this.getSingleEntry = param => {
     return $http({
       method: 'GET',
       url: '/api/singleEntry/' + param + '/'
-    }).then(response => response.data[0])
-  }
+    }).then(response => response.data[0]);
+  };
 
   this.userAddEntry = (entry, id) => {
     // console.log("mainSrvc", id)
@@ -99,8 +96,6 @@ angular.module('myApp').service('mainSrvc', function($http, $rootScope) {
     }).then(response => {
       // console.log(response)
       return response.data;
-    })
+    });
   };
-
-
-})
+});
